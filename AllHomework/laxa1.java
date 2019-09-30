a29f4dacc814fe948b453b4d9647fb761a02bca5
lasholof <lucas.lindqvistorder@gmail.com>
2019-09-30 13:41:34 +0200

    init

---
+++ AllHomework/laxa1.java
@@ -0,0 +1,47 @@
+import java.util.Scanner;
+
+public class laxa1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Vad heter du?");
+
+		Scanner namnInput = new Scanner(System.in);
+		String namn = namnInput.nextLine();
+
+		System.out.println("Hur gammal är du?");
+
+		Scanner ålderInput = new Scanner(System.in);
+		String ålder = namnInput.nextLine();
+
+		System.out.println("Vad är din adress?");
+
+		Scanner adressInput = new Scanner(System.in);
+		String adress = adressInput.nextLine();
+
+		System.out.println("Vad är ditt postnummer?");
+
+		Scanner postnummerInput = new Scanner(System.in);
+		String postnummer = postnummerInput.nextLine();
+
+		System.out.println("Vad är ditt telefonnummer?");
+
+		Scanner telefonnummerInput = new Scanner(System.in);
+		String telefonnummer = adressInput.nextLine();
+
+		System.out.println("Information" + "\n");
+
+		System.out.println("Namn:Lucas" + "\n");
+
+		System.out.println("17 år" + "\n");
+
+		System.out.println("Adress:Valebergsvägen" + "\n");
+
+		System.out.println("Postnummer:42737" + "\n");
+
+		System.out.println("Telefonnummer:0723376320" + "\n");
+
+	}
+
+}
