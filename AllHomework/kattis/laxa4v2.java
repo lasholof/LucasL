0debcfd04aa8c6fc24a09834f74f1a12f1f61526
lasholof <lucas.lindqvistorder@gmail.com>
2019-10-07 13:04:19 +0200

    Update laxa4v2.java

--- AllHomework/kattis/laxa4v2.java
+++ AllHomework/kattis/laxa4v2.java
@@ -9,7 +9,28 @@ int x = sc.nextInt();
         int y = sc.nextInt();
         
         int n = sc.nextInt();
-        
+        for (int i = 1; i <= n; i++) {
+        	
+            if (i % x == 0 && i % y == 0) {
+            	
+                System.out.println("FizzBuzz");
+                
+            } else if (i % x == 0) {
+            	
+                System.out.println("Fizz");
+                
+            } else if (i % y == 0) {
+            	
+                System.out.println("Buzz");
+                
+            } else {
+            	
+                System.out.println(i);
+            }
+            
+                
+            
+        }
 	}
 
 }
