fd597de59736d39c6ebda533b2359779d0eae136
lasholof <lucas.lindqvistorder@gmail.com>
2019-10-07 12:57:44 +0200

    Update laxa4.java

--- AllHomework/kattis/laxa4.java
+++ AllHomework/kattis/laxa4.java
@@ -1,12 +1,16 @@
-package kattis;
-
 import java.util.Scanner;
 
 public class laxa4 {
-	
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		
-		Scanner input
+
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+
+        Scanner input = new Scanner(System.in);
+
+        int hour = input.nextInt();
+        int minute = input.nextInt();
+
+        minute = minute - 45;
+
 	}
 }
