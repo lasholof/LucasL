432ab0f3c74483bd71f0be2ade557d47258dbc52
lasholof <lucas.lindqvistorder@gmail.com>
2019-10-07 13:03:42 +0200

    Update laxa4v2.java

--- AllHomework/kattis/laxa4v2.java
+++ AllHomework/kattis/laxa4v2.java
@@ -4,7 +4,12 @@ public class laxa4v2 {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-
+int x = sc.nextInt();
+        
+        int y = sc.nextInt();
+        
+        int n = sc.nextInt();
+        
 	}
 
 }
